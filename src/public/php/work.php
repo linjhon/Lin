817731bb53322c0ba71dae5e96789168c758bc31
linqiang <461276106@qq.com>
2017-10-09 15:13:59 +0800

    1

---
+++ src/public/php/work.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @Author: Marte
+ * @Date:   2017-03-11 21:06:57
+ * @Last Modified by:   Marte
+ * @Last Modified time: 2017-03-18 22:23:15
+ */
+  header('content-type:application:json;charset=utf8');
+	header('Access-Control-Allow-Origin:*');
+	header('Access-Control-Allow-Methods:POST');
+	header('Access-Control-Allow-Headers:x-requested-with,content-type');
+  include('conn.php');
+  if($sql){
+    $db = "SELECT * FROM work";
+
+    $result = mysql_query($db);
+    $arr = array();
+    $results = array();
+    while ($row = mysql_fetch_assoc($result)) {
+      $results[] = $row;
+    }
+    echo json_encode($results);
+    mysql_close($sql);
+  }else{
+    echo false;
+  }
